f298c917c2b11907d907479bb08ebb8a0b81266c
Lorenz Ras <lenzras@gmail.com>
2025-06-08 14:28:48 +0900

    add drill

---
+++ JavaDrills/answers/drill01/VariableDeclaration3.java
@@ -0,0 +1,18 @@
+package drill01_variable_declaration;
+
+/*
+* double 型の変数 temperature を用意してください。
+* 次に、temperature に 36.5 を代入してください。
+* System.out.println を使って、temperature の値を出力してください。
+*
+*/
+
+public class VariableDeclaration3 {
+  public static void main(String[] args) {
+    // ここに書いてください
+    double temperature;
+    temperature = 36.5;
+
+    System.out.println(temperature);
+  }
+}
