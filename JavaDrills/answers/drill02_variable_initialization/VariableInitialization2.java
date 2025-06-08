7ed17775798b870526fbec2705656bff025afd85
Lorenz Ras <lenzras@gmail.com>
2025-06-08 15:03:10 +0900

    add drill

---
+++ JavaDrills/answers/drill02_variable_initialization/VariableInitialization2.java
@@ -0,0 +1,17 @@
+package drill02_variable_initialization;
+
+/*
+* 単一の文字型の変数 grade を 1 行で記述（初期化）してください。
+* 値は A で初期化してください。
+* System.out.println を使って、grade の値を出力してください。
+*
+*/
+
+public class VariableInitialization2 {
+	public static void main(String[] args) {
+		// ここに書いてください
+		char grade = 'A';
+
+		System.out.println(grade);
+	}
+}
