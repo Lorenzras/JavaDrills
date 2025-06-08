7226b254764f27d4f4a39e7f51fd34fbd666d200
Lorenz Ras <lenzras@gmail.com>
2025-06-09 01:15:15 +0900

    add drill

--- JavaDrills/src/drill08_for_continue/ForContinue5.java
+++ JavaDrills/src/drill08_for_continue/ForContinue5.java
@@ -1,5 +1,14 @@
 package drill08_for_continue;
 
-public class ForContinue5 {
+/*
+ * 整数を 1 ～ 10 まで出力するループを書いてください。
+ * ただし、3 の倍数のときはスキップして出力しないように continue 文を使ってください。
+ */
 
+public class ForContinue5 {
+	public static void main(String[] args) {
+
+		// ここにfor文を書いてください
+
+	}
 }
--- JavaDrills/test/drill08_for_continue/ForContinue5Test.java
+++ JavaDrills/test/drill08_for_continue/ForContinue5Test.java
@@ -1,14 +1,36 @@
 package drill08_for_continue;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
-class ForContinue5Test {
+import utils.SourceCodeAssert;
+
+public class ForContinue5Test {
 
 	@Test
-	void test() {
-		fail("まだ実装されていません");
+	public void testOutput() throws Exception {
+		String expected = String.join(System.lineSeparator(),
+				"1",
+				"2",
+				"4",
+				"5",
+				"7",
+				"8",
+				"10");
+		SourceCodeAssert.assertOutputMatches(getClass(), expected);
 	}
 
+	@Test
+	public void testIfUsed() throws Exception {
+		SourceCodeAssert.assertIfUsed(getClass());
+	}
+
+	@Test
+	public void testContinueUsed() throws Exception {
+		SourceCodeAssert.assertContinueUsed(getClass());
+	}
+
+	@Test
+	public void testForUsed() throws Exception {
+		SourceCodeAssert.assertForUsed(getClass());
+	}
 }
