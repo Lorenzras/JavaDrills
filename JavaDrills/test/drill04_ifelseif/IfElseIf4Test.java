f1451abb1a2c14675b066b14fcf8127c1a5cbbc5
Lorenz Ras <lenzras@gmail.com>
2025-06-08 18:44:05 +0900

    add drill

---
+++ JavaDrills/test/drill04_ifelseif/IfElseIf4Test.java
@@ -0,0 +1,40 @@
+package drill04_ifelseif;
+
+import org.junit.jupiter.api.Test;
+
+public class IfElseIf4Test {
+
+    @Test
+    public void testOutput36_9() throws Exception {
+        testWithValue(36.9, "平熱");
+    }
+
+    @Test
+    public void testOutput37_0() throws Exception {
+        testWithValue(37.0, "微熱");
+    }
+
+    @Test
+    public void testOutput37_9() throws Exception {
+        testWithValue(37.9, "微熱");
+    }
+
+    @Test
+    public void testOutput38_0() throws Exception {
+        testWithValue(38.0, "高熱");
+    }
+
+    @Test
+    public void testOutput39_0() throws Exception {
+        testWithValue(39.0, "高熱");
+    }
+
+    @Test
+    public void testIfElseUsed() throws Exception {
+        IfElseTestUtil.assertIfElseUsed(getClass());
+    }
+
+    private void testWithValue(double temperature, String expected) throws Exception {
+        IfElseTestUtil.assertOutputMatches(getClass(), "temperature", String.valueOf(temperature), expected);
+    }
+}
