3a7c019c455f3ad84a7427d91ab4b1a3b9370d07
Lorenz Ras <lenzras@gmail.com>
2025-06-08 15:07:49 +0900

    add drill

---
+++ JavaDrills/answers/drill02_variable_initialization/VariableInitialization4.java
@@ -0,0 +1,17 @@
+package drill02_variable_initialization;
+
+/*
+* double 型の変数 weight を 1 行で記述（初期化）してください。
+* 値は 58.7 にしてください。
+* System.out.println を使って、weight の値を出力してください。
+*
+*/
+
+public class VariableInitialization4 {
+	public static void main(String[] args) {
+		// ここに書いてください
+		double weight = 58.7;
+
+		System.out.println(weight);
+	}
+}
