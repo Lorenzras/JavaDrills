bc7643c23d13461c4434f6a026e26166e5307fc0
lenzras <lenzras@gmail.com>
2025-06-23 16:53:23 +0900

    add drill

--- JavaDrills/src/drill10_arraylist_for/ArrayListFor4.java
+++ JavaDrills/src/drill10_arraylist_for/ArrayListFor4.java
@@ -2,18 +2,12 @@ package drill10_arraylist_for;
 
 /*
  * 【問題】
- * ArrayList に (9, 8, 7, 6, 5) を追加し、
- * 普通の for 文（拡張 for ではなく）で要素を逆順に出力してください。
+ * ArrayList に (100, 200, 300, 400) を追加し、
+ * 合計値を for 文（拡張 for ではなく）で計算して出力してください。
  *
  * 【期待される出力】
- * 5
- * 6
- * 7
- * 8
- * 9
+ * 1000
  */
-
-import java.util.ArrayList;
 
 public class ArrayListFor4 {
     public static void main(String[] args) {
